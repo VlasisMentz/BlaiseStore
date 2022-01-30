edcea837922c58e9f953eac9942e7bd7baa54ea3
Vlasis Mentz <vlasism@gmail.com>
2022-01-30 15:14:53 +0200

    fix bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import styled from 'styled-components';
-import logo from '../assets/logo.svg';
 import logo2 from '../assets/logo2.png';
 import { FaBars } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
